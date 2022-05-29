fa53cd6edaca5388e6c5bc5cfd170d62e4f46dc1
dilshod996 <dnurmamatoff@gmail.com>
2022-05-29 19:34:07 +0900

    RestartingGame(SceneManagaer)

--- Assets/Scripts/Crashed.cs
+++ Assets/Scripts/Crashed.cs
@@ -1,9 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Crashed : MonoBehaviour
 {
+    // [SerializeField] gameObject destroyFuel;
+    // [SerializeField] float delayedTime= 1f;
+    // void DestroyObjectDelayed() {
+    //     Destroy(gameObject, delayedTime);
+    // }
     void OnCollisionEnter(Collision other) {
         string tagName = other.gameObject.tag;
         switch (tagName)
@@ -16,9 +22,11 @@ public class Crashed : MonoBehaviour
                 break; 
             case "Fuel":
                 Debug.Log("You picked up fuel");
+                // DestroyObjectDelayed();
                 break;
             default:
                 Debug.Log("Please dont touch me");
+                SceneManager.LoadScene("StartScene");
                 break;
 
         }
